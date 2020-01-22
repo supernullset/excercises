1c420e77c727a9859f4580c1485819e69bb6ee3e
supernullset <sean.j.williamson@gmail.com>
2020-01-21 17:06:56 -0800

    more wips

---
+++ Chapter2-SICP.rkt
@@ -0,0 +1,102 @@
+#lang sicp
+
+(display "section 2.1")
+(newline)
+
+;; Ex 2.1 made this handle negative sign as well
+(define (make-rat n d)
+  (let ((g ((if (< d 0) - +) (abs (gcd n d)))))
+    (cons (/ n g) (/ d g))))
+
+(define (numer r)
+  (car r))
+
+(define (denom r)
+  (cdr r))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (denom x) (numer y)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom y) (denom x))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (numer y) (denom x))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+
+(print-rat one-half)
+
+(define one-third (make-rat 1 3))
+
+(print-rat (add-rat one-half one-third))
+
+(print-rat (mul-rat one-half one-third))
+
+(print-rat (add-rat one-third one-third))
+
+(print-rat (make-rat (- 1) (- 7)))
+
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (make-segment p1 p2)
+  (cons p1 p2))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+(define (midpoint-segment seg)
+  (let ((p1 (start-segment seg))
+        (p2 (end-segment seg)))
+    (let ((x1 (x-point p1))
+          (y1 (y-point p1))
+          (x2 (x-point p2))
+          (y2 (y-point p2)))
+      (make-point (/ (+ x1 x2) 2) (/ (+ y1 y2) 2)))))
+
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(newline)
+(display "----INTERVAL ARITHMETIC----")
+
+
+
+(newline)
+(display "----END OF SECTION----")
---
+++ intervals.rkt
@@ -0,0 +1,99 @@
+#lang sicp
+
+
+;; Electrical engineers will be using Alyssa’s system to compute
+;; electrical quantities. It is sometimes necessary for them to compute
+;; the value of a parallel equivalent resistance R p of two resistors R 1
+;; and R 2 using the formula
+;;
+;; R p = 1 / (1 / R1 + 1 / R2)
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x)
+                    (lower-bound y))
+                 (+ (upper-bound x)
+                    (upper-bound y))))
+
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x)
+               (lower-bound y)))
+        (p2 (* (lower-bound x)
+               (upper-bound y)))
+        (p3 (* (upper-bound x)
+               (lower-bound y)))
+        (p4 (* (upper-bound x)
+               (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (if (or (= 0 (width-interval x)) (= 0 (width-interval y)))
+      (error "Division error (interval spans 0)" y)
+      (mul-interval x
+                (make-interval
+                 (/ 1.0 (upper-bound y))
+                 (/ 1.0 (lower-bound y))))))
+
+(define (make-interval a b) (cons a b))
+
+(define lower-bound car)
+
+(define upper-bound cdr)
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x)
+                    (upper-bound y))
+                 (- (upper-bound x)
+                    (lower-bound y))))
+
+(define (sub-test)
+  (let ((i1 (make-interval 1 1))
+        (i2 (make-interval 0 1))
+      (i3 (make-interval -1 0)))
+    (sub-interval i3 i1)))
+
+
+(define (width-interval i)
+  (abs (/ (- (upper-bound i) (lower-bound i)) 2)))
+
+(define i1 (make-interval 1 1))
+(define i2 (make-interval 0 1))
+(define i3 (make-interval -1 0))
+
+(define i4 (make-interval -1 1))
+(define i5 (make-interval 4 9))
+
+(define (width-proof)
+  (display "addition: ")
+  (display (=
+            (+ (width-interval i1) (width-interval i2))
+            (width-interval (add-interval i1 i2))
+            ))
+
+  (newline)
+  (display "subtraction: ")
+  (display (=
+            ;; width should be a scalar, no need for direction here
+            (abs (- (width-interval i1) (width-interval i2)))
+            (width-interval (sub-interval i1 i2))
+            ))
+
+  (newline)
+  (display "multiplication: ")
+  (display (=
+            ;; width should be a scalar, no need for direction here
+            (abs (- (width-interval i2) (width-interval i3)))
+            (width-interval (mul-interval i2 i3))
+            ))
+
+  (newline)
+  (display "division: ")
+  (display (=
+            ;; width should be a scalar, no need for direction here
+            (abs (- (width-interval i4) (width-interval i5)))
+
+            ;; Note that unit intervals dont play nice with the division algorithm here - div by zero is possible
+            (width-interval (div-interval i4 i5))
+            ))
+  )
